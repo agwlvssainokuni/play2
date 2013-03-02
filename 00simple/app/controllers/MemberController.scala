30b14b4d5c2e255e8b058e551a64df8d286ff108
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2013-03-02 20:32:35 +0900

    Play2 Scalaアプリ - simple * まずはコントローラだけ作成 (routesも)

---
+++ 00simple/app/controllers/MemberController.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import play.api.mvc.Controller
+
+object MemberController extends Controller {
+
+  def list() = TODO
+
+  def fresh() = TODO
+
+  def create() = TODO
+
+  def show(id: Int) = TODO
+
+  def edit(id: Int) = TODO
+
+  def update(id: Int) = TODO
+
+  def delete(id: Int) = TODO
+
+}
