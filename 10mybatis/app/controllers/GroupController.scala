db9ff5a5cbe5b0411c9dad62cb923c283497815b
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2013-03-09 00:34:00 +0900

    Play2 Scalaアプリ - mybatis * コントローラを作成 * ルーティング定義を追加

---
+++ 10mybatis/app/controllers/GroupController.scala
@@ -0,0 +1,27 @@
+package controllers
+
+import models._
+import play.api.Play.current
+import play.api.data._
+import play.api.data.Forms._
+import play.api.db._
+import play.api.mvc._
+import views.html
+
+object GroupController extends Controller {
+
+  def list() = TODO
+
+  def fresh() = TODO
+
+  def create() = TODO
+
+  def show(id: Int) = TODO
+
+  def edit(id: Int) = TODO
+
+  def update(id: Int) = TODO
+
+  def delete(id: Int) = TODO
+
+}
---
+++ 10mybatis/env.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+@SET _JAVA_OPTIONS="-Dfile.encoding=Windows-31J"
